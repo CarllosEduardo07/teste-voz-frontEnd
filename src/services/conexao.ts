ae99f12df92251a42f3391d6c4fe54ae5028694f
Carlos Eduardo - PC_Casa <eduardo201184@hotmail.com>
2025-01-11 20:18:02 -0300

    feature: add tryCatch nas chamadas da API

--- src/services/conexao.ts
+++ src/services/conexao.ts
@@ -6,25 +6,43 @@ const api = axios.create({
 });
 
 export const getComments = async (): Promise<newCommentData[]> => {
-  const response = await api.get('/comentarios');
-  return response.data;
+  try {
+    const response = await api.get('/comentarios');
+    return response.data;
+  } catch (error) {
+    console.log('Erro ao carragar os dados do servidor', error);
+    return [];
+  }
 };
 
 export const createComment = async (
   newComment: newCommentData,
 ): Promise<newCommentData> => {
-  const response = await api.post('/comentarios', newComment);
-  return response.data;
+  try {
+    const response = await api.post('/comentarios', newComment);
+    return response.data;
+  } catch (error) {
+    console.log(
+      'Erro ao enviar os dados do novo comentário para o servidor.',
+      error,
+    );
+    throw error;
+  }
 };
 
 export const updateComment = async (
   updateComment: newCommentData,
 ): Promise<newCommentData> => {
-  const response = await api.put(
-    `/comentarios/${updateComment.id}`,
-    updateComment,
-  );
-  return response.data;
+  try {
+    const response = await api.put(
+      `/comentarios/${updateComment.id}`,
+      updateComment,
+    );
+    return response.data;
+  } catch (error) {
+    console.log('Erro ao enviar os dados atualizados para o servidor.', error);
+    throw error;
+  }
 };
 
 export const deleteComment = async (id: string) => {
@@ -32,6 +50,7 @@ export const deleteComment = async (id: string) => {
     const response = await api.delete(`/comentarios/${id}`);
     return response.data;
   } catch (error) {
-    console.error('Erro ao deletar o comentario', error);
+    console.error('Erro ao Deletar o comentário  no servidor.', error);
+    throw error;
   }
 };
